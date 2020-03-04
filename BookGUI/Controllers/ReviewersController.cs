89f74a15e1d6dec948f37409fded957a9a47737f
Ted <w24351789@gmail.com>
2020-03-04 23:28:07 +0800

    03042328

--- BookGUI/Controllers/ReviewersController.cs
+++ BookGUI/Controllers/ReviewersController.cs
@@ -11,10 +11,12 @@ namespace BookGUI.Controllers
     public class ReviewersController : Controller
     {
         private readonly IReviewerRepositoryGUI _reviewerRepository;
+        private readonly IReviewRepositoryGUI _reviewRepository;
 
-        public ReviewersController(IReviewerRepositoryGUI reviewerRepository)
+        public ReviewersController(IReviewerRepositoryGUI reviewerRepository, IReviewRepositoryGUI reviewRepository)
         {
             _reviewerRepository = reviewerRepository;
+            _reviewRepository = reviewRepository;
         }
 
         public async Task<ActionResult> Index()
@@ -31,12 +33,26 @@ namespace BookGUI.Controllers
         
         public async Task<ActionResult> GetReviewerById(int reviewerId)
         {
+            //欲顯示reviewer 以及其每則review跟所評論的Book
             var reviewer = await _reviewerRepository.GetReviewerById(reviewerId);
             if(reviewer == null)
             {
                 ViewBag.Message = $"Did not have {reviewerId}'s Id reviewer";
                 ModelState.AddModelError("", "Some Error getting reviewer");
                 reviewer = new ReviewerDto();
+            }
+            //取得
+            var reviews = await _reviewerRepository.GetReviewsByAReviewer(reviewerId);
+            if(reviews.Count() <= 0)
+            {
+                ViewBag.ReviewMessage = $"{reviewer.FirstName} {reviewer.LastName} did not have any reviews.";
+            }
+
+            IDictionary<ReviewDto, BookDto> reviewAndBook = new Dictionary<ReviewDto, BookDto>();
+            foreach(var review in reviews)
+            {
+                var book = await _reviewRepository.GetBookForAReview(review.Id);
+                reviewAndBook.Add(review, book);
             }
 
             return View(reviewer);
---
+++ BookGUI/ViewModels/ReviewerReviewsBooksViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BookGUI.Services.ModelDTOs;
+using System.Collections.Generic;
+
+namespace BookGUI.ViewModels
+{
+    public class ReviewerReviewsBooksViewModel
+    {
+        public ReviewerDto  Reviewer { get; set; }
+        public IDictionary<ReviewDto, BookDto> ReviewAndBook { get; set; }
+    }
+}
